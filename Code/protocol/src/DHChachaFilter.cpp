6e53e1a9188efdfb48990a92860173518fdad171
Maxime Griot <yamashi@live.com>
2019-06-12 18:17:49 +0200

    Replace DH with ECDH

--- Code/protocol/src/DHChachaFilter.cpp
+++ Code/protocol/src/DHChachaFilter.cpp
@@ -4,33 +4,24 @@
 #include "chacha.h"
 #include "integer.h"
 #include "dh.h"
+#include "eccrypto.h"
+#include "oids.h"
 #include "osrng.h"
 #include "secblock.h"
 #include "sha.h"
 #include "blake2.h"
+#include "asn.h"
 
-namespace DHParams
-{
-    static CryptoPP::Integer p("0xB10B8F96A080E01DDE92DE5EAE5D54EC52C99FBCFB06A3C6"
-        "9A6A9DCA52D23B616073E28675A23D189838EF1E2EE652C0"
-        "13ECB4AEA906112324975C3CD49B83BFACCBDD7D90C4BD70"
-        "98488E9C219A73724EFFD6FAE5644738FAA31A4FF55BCCC0"
-        "A151AF5F0DC8B4BD45BF37DF365C1A65E68CFDA76D4DA708"
-        "DF1FB2BC2E4A4371");
 
-    static CryptoPP::Integer g("0xA4D1CBD5C3FD34126765A442EFB99905F8104DD258AC507F"
-        "D6406CFF14266D31266FEA1E5C41564B777E690F5504F213"
-        "160217B4B01B886A5E91547F9E2749F4D7FBD7D3B9A92EE1"
-        "909D0D2263F80A76A6A24C087A091F531DBF0A0169B6A28A"
-        "D662A4D18E73AFA32D779D5918D08BC8858F4DCEF97C2A24"
-        "855E6EEB22B3B2E5");
-
-    static CryptoPP::Integer q("0xF518AA8781A8DF278ABA4E7D64B7CB9D49462353");
-}
+using namespace CryptoPP::ASN1;
 
 struct DHChachaFilterPimpl
 {
-    CryptoPP::DH m_dh;
+    DHChachaFilterPimpl()
+        : m_dh(secp256r1())
+    {}
+
+    CryptoPP::ECDH<CryptoPP::ECP>::Domain m_dh;
     CryptoPP::XChaCha20::Encryption m_cipher;
     CryptoPP::SecByteBlock m_pubKey;
     CryptoPP::SecByteBlock m_priKey;
@@ -40,8 +31,6 @@ DHChachaFilter::DHChachaFilter()
     : m_pPimpl{GetAllocator()->New<DHChachaFilterPimpl>()}
     , m_iv{0}
 {
-    m_pPimpl->m_dh.AccessGroupParameters().Initialize(DHParams::p, DHParams::q, DHParams::g);
-
     m_pPimpl->m_priKey.resize(m_pPimpl->m_dh.PrivateKeyLength());
     m_pPimpl->m_pubKey.resize(m_pPimpl->m_dh.PublicKeyLength());
 
@@ -113,7 +102,7 @@ bool DHChachaFilter::PreReceive(uint8_t* apPayload, size_t aLength, uint32_t aSe
 
 void DHChachaFilter::GenerateKeys()
 {
-    CryptoPP::AutoSeededRandomPool rng;
+    CryptoPP::AutoSeededX917RNG<CryptoPP::AES> rng;
 
     m_pPimpl->m_dh.GenerateKeyPair(rng, m_pPimpl->m_priKey, m_pPimpl->m_pubKey);
 }
